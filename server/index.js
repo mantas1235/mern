1e3f02a2cd60496f16aff507df5e40e31940bc5e
mantas1235 <mantas.san@gmail.com>
2024-10-19 12:50:02 +0300

    test api route created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 
+import userRoutes from './routes/user.route.js'
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=> {
@@ -18,3 +20,6 @@ app.listen(3000, ()=> {
     console.log('server at 3000!');
     
 });
+
+
+app.use('/api/user' , userRoutes);
