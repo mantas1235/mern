afb2595802a60928523dd3dbcfd5ff6f8095a44b
mantas1235 <mantas.san@gmail.com>
2024-10-19 10:47:59 +0300

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Home from "./assets/pages/Home"
 import About from './assets/pages/About';
 import Signin from './assets/pages/Signin';
 import SignUp from './assets/pages/SignUp';
-import Profile from './assets/pages/SignUp';
+import Profile from './assets/pages/Profile';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/about' element={<About />}></Route>
